5c43746c481fb59e394c8f59caa6610258379aed
engi-devops <singhsantosh.edu.in@gmail.com>
2024-02-11 09:13:37 +0530

    fixed

--- app/views.py
+++ app/views.py
@@ -4,23 +4,23 @@ from app.controllers import register_user, register_all_user, get_register_user,
 from app.models import tbl_user
 
 @app.route('/user', methods=['GET'])
-def get_items():
+def register_all_user():
     return jsonify(register_all_user())
 
 @app.route('/user/<int:user_id>', methods=['GET'])
-def get_single_item(user_id):
+def get_register_user(user_id):
     return jsonify(get_register_user(user_id))
 
 @app.route('/user', methods=['POST'])
-def add_item():
+def register_user():
     data = request.get_json()
     return jsonify(register_user(data))
 
 @app.route('/user/<int:user_id>', methods=['PUT'])
-def update_single_item(user_id):
+def update_register_user(user_id):
     data = request.get_json()
     return jsonify(update_register_user(user_id, data))
 
 @app.route('/user/<int:user_id>', methods=['DELETE'])
-def delete_single_item(user_id):
+def delete_register_user(user_id):
     return jsonify(delete_register_user(user_id))
